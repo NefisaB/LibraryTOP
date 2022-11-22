a7e1ea96bcbbf9c28d82b3b55b93665706c5c042
NefisaB <nefisa.hc@gmail.com>
2022-11-22 15:28:01 +0100

    Add delete book implementation.

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const form = document.querySelector("form");
 const titleEl = document.getElementById("title");
 const authorEl = document.getElementById("author");
 const pagesEl = document.getElementById("pages");
-const isReadEl = document.querySelector(".isRead");
-
-
-const myLibrary = [];
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -18,30 +14,30 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
-function addBookToTheLibrary(book) {
-    myLibrary.push(book);
-}
-
-const book1 = new Book("Robinson Cruose", "Daniel Deffoe", 155, false);
-const book2 = new Book("Robinson Cruose", "Daniel Deffoe", 155, true);
-
-addBookToTheLibrary(book1);
-addBookToTheLibrary(book2);
+let myLibrary = [new Book("Helo", "Hi", 3, false)];
 
 function addBookToList(book) {
-
     const h3 = document.createElement("h3");
     h3.textContent = book.title;
     const h4 = document.createElement("h4");
     h4.textContent = book.author;
     const readButton = document.createElement("button");
     readButton.classList.add("read-button");
-    readButton.textContent = book.isRead ? "read" : "unread";
+    console.log(book.isRead);
+    readButton.innerText = book.isRead ? "read" : "unread";
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
+    deleteBtn.classList.add("delete-button");
+    deleteBtn.setAttribute("onclick", `deleteBook("${book.title}")`);
+    const buttonsDiv = document.createElement("div");
+    buttonsDiv.classList.add("buttons-div");
+    buttonsDiv.appendChild(readButton);
+    buttonsDiv.appendChild(deleteBtn);
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book-container");
     bookDiv.appendChild(h3);
     bookDiv.appendChild(h4);
-    bookDiv.appendChild(readButton);
+    bookDiv.appendChild(buttonsDiv);
     booksContainer.insertBefore(bookDiv, newBtnDiv);
 }
 
@@ -49,7 +45,12 @@ function loadLibrary() {
     for (let book of myLibrary) {
         addBookToList(book);
     }
+}
 
+function deleteBook(title) {
+    myLibrary = myLibrary.filter((book) => book.title !== title);
+    booksContainer.replaceChildren(newBtnDiv);
+    loadLibrary();
 }
 
 function hideForm() {
@@ -64,14 +65,13 @@ addBookBtn.addEventListener("click", function () {
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const newBook = new Book(titleEl.value, authorEl.value, pagesEl.value, isReadEl.value);
+    const isReadString = document.querySelector('input[name="isRead"]:checked').value;
+    const isRead = (isReadString === "true");
+    const newBook = new Book(titleEl.value, authorEl.value, pagesEl.value, isRead);
     myLibrary.push(newBook);
-    console.log(newBook);
     addBookToList(newBook);
     hideForm();
     form.reset();
 });
-
-
 
 loadLibrary();
